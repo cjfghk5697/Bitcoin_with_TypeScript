0ea7e1d08f75522aaf3e18414352746af3b9f424
cjfghk5697 <cjfghk5697@gmail.com>
2022-01-06 01:16:56 +0900

    second

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hi");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Han", age = 21, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name}, you are ${age}, gender ${gender}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
