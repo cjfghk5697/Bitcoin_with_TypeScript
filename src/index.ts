d782c90fd1bc380558dcab22cef6e8d7cae8de7b
cjfghk5697 <cjfghk5697@gmail.com>
2022-01-06 14:22:13 +0900

    final basic, start bitcoin

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+class Human{
+    public name: string;
+    public age: number;
+    private gender:string; //private형으로 클래스 내부에서만 사용가능
+    constructor(name:string,age:number,gender?:string){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
+}
+const lynn=new Human("Han",18,"male");
 
+
+const sayHi=(person:Human) : string=>{
+    return `hello ${person.name}, you are ${person.age}, gender ${person.gender}`
+};  //각각 어떤 객체를 가지고 있는지 .name, .age들으로 알수 있음.
+sayHi(lynn); 
 export {};
-
-
-const sayHi=(name:string,age:number,gender:string) : string=>{
-    return `hello ${name}, you are ${age}, gender ${gender}`
-}; // : string을 추가하여 string을 return하는 것을 알려줌
-//argument 형에 맞게 알려줌
-sayHi("han", 444,"male"); //tslint 확장앱으로 뭐가 잘못되면 오류 알려줌
